cde41d0703aee4d8f8bf196e667fb149be8f69e1
sagostinelli <batonenbois@gmail.com>
2021-03-24 18:02:49 -0400

    greetings update

--- src/github_action_assets/public/index.js
+++ src/github_action_assets/public/index.js
@@ -1,5 +1,5 @@
 import github_action from 'ic:canisters/github_action';
 
-github_action.greet(window.prompt("Enter your nameasdasdadas:")).then(greeting => {
+github_action.greet(window.prompt("Enter your cool name:")).then(greeting => {
   window.alert(greeting);
 });
